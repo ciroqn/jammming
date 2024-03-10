2c6b3efed33b5304a20cf9837daeb3a15cbf74b1
mwatson <noreply>
2024-03-10 17:29:08 +0000

    add render function in Track that creates removal btn

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./Track.module.css";
 
-function Track() {
+function Track(props) {
+  function renderAction() {
+    return <button className="Track-action">{props.isRemoval ? '-' :  '+'}</button>
+  }
+
   return (
     <div className={styles.Track}>
       <div className={styles.Track-information}>
         <h3>track name will go here</h3>
         <p>track artist will go here | track album will go here</p>
       </div>
-      <button className="Track-action">{isRemoval ? '-' :  '+'}</button>
+      renderAction();
     </div>
   );
 }
