4dfdd8aecf24afc008f23dd7acaba81f31966e57
mwatson <noreply>
2024-03-10 15:25:16 +0000

    add SearchResults component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SearchResults.css";
+
+function SearchResults() {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList />
+    </div>
+  );
+}
+
+export default SearchResults;
